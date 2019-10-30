d88694e1495cf6bca827f305272ca7829fdccf7a
alexkat20 <alexkat20@rambler.ru>
2019-10-30 21:26:48 +0300

    Task-2

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -2,9 +2,40 @@ package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
 
+import java.sql.ResultSet;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ResultTreeType;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        return resultSet -> {
+            try{
+                Employee emp = new Employee(new BigInteger(resultSet.getString("ID")), new FullName(
+                        resultSet.getString("FIRSTNAME"), resultSet.getString("LASTNAME"), resultSet.getString("MIDDLENAME")
+                ), Position.valueOf(resultSet.getString("POSITION")),
+                        LocalDate.parse(resultSet.getString("HIREDATE")),
+                        new BigDecimal(resultSet.getString("SALARY"))
+                        );
+                return emp;
+
+            } catch (SQLException e) {
+                return null;
+            }
+
+        };
+
+
     }
 }
